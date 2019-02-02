5cdb2b7fda11fbe53b0a3867000d65c14c444c01
liting039 <1798640602@qq.com>
2019-02-03 00:21:59 +0800

    Update main.js

--- mvc02/src/main.js
+++ mvc02/src/main.js
@@ -3,7 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// import '@/assets/css/index.css'
+import myaxios from './assets/js/myaxios.js'
 
+
+Vue.use(myaxios)
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
