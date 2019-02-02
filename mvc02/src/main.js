b85db48004417a021ff19b7c31aed27d01626567
liting039 <1798640602@qq.com>
2019-02-03 01:33:20 +0800

    Update main.js

--- mvc02/src/main.js
+++ mvc02/src/main.js
@@ -5,7 +5,7 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-// import '@/assets/css/index.css'
+import '@/assets/css/common.css'
 import myaxios from './assets/js/myaxios.js'
 
 
