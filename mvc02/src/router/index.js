61934588d79daa906280e9258bdd37e82b1ab75d
liting039 <1798640602@qq.com>
2019-02-03 15:02:15 +0800

    Update index.js

--- mvc02/src/router/index.js
+++ mvc02/src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
 import Login from '@/components/login/login.vue'
 import Home from '@/components/pages/home.vue'
+import User from '@/components/pages/users/user.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -15,7 +16,14 @@ export default new Router({
     {
       name:'home',
       path:'/home',
-      component:Home  
-    }
+      component:Home,
+      children:[
+        {
+          path:'/user',
+          component:User
+        }
+      ]
+    },
+   
   ]
 })
