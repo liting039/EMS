fc49aac9dcd3b5ac1d37d8e79d37d37e3c2fdcd2
liting039 <1798640602@qq.com>
2019-02-03 13:59:41 +0800

    Create top.vue

---
+++ mvc02/src/components/layout/top.vue
@@ -0,0 +1,29 @@
+<template>
+    <el-row>
+        <el-col :span="6">
+            <div>
+                <img src="@/assets/images/logo.png" alt="">
+            </div>
+        </el-col>
+        <el-col :span="12">
+            <div class="txt">
+                电商后台管理系统
+            </div>
+        </el-col>
+        <el-col :span="6">
+            <div>
+                <a href="#">退出</a>
+            </div>
+        </el-col>
+    </el-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
